661e90a4b02aaeff45f23081730d9d30727570fa
kerdo1 <kerdovahk@gmail.com>
2024-10-01 17:42:27 +0300

    Base
    original

---
+++ MoneyManager/KlassiRahaHaldamine/KlassiRahaHaldamine/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KlassiRahaHaldamine
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
