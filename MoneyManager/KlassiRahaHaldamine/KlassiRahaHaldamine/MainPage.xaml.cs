01654b2384abd9cdce704f462410fb1255f09560
PiaLember <pia.lember@gmail.com>
2024-10-09 17:53:50 +0300

    Add Students money sum calculations and show on Mainpage

--- MoneyManager/KlassiRahaHaldamine/KlassiRahaHaldamine/MainPage.xaml.cs
+++ MoneyManager/KlassiRahaHaldamine/KlassiRahaHaldamine/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using KlassiRahaHaldamine.Views;
+﻿using KlassiRahaHaldamine.Data;
+using KlassiRahaHaldamine.Models;
+using KlassiRahaHaldamine.Views;
 using KlassiRahaHaldamine.Views.Events;
 using KlassiRahaHaldamine.Views.Students;
 using Microsoft.Extensions.Logging;
@@ -7,11 +9,25 @@ namespace KlassiRahaHaldamine
 {
     public partial class MainPage : ContentPage
     {
-
+        private DatabaseContext _databaseContext;
 
         public MainPage()
         {
             InitializeComponent();
+            _databaseContext = new DatabaseContext();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await DisplayTotalAmountAsync();
+        }
+        private async Task DisplayTotalAmountAsync()
+        {
+            var students = await _databaseContext.GetAllAsync<Student>(); 
+            decimal totalAmount = students.Sum(student => student.Amount); 
+
+            MoneyLabel.Text = $"{totalAmount:C}";
         }
 
         async void OnStudentsClicked(System.Object sender, System.EventArgs e)
