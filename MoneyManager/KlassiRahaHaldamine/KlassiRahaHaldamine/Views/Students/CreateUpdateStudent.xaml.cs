d9cd7de514564428bce8f7e39161d31ce9fe00aa
4use2me <forusetome@gmail.com>
2024-10-11 00:43:14 +0300

    StudentsRequiredFields

--- MoneyManager/KlassiRahaHaldamine/KlassiRahaHaldamine/Views/Students/CreateUpdateStudent.xaml.cs
+++ MoneyManager/KlassiRahaHaldamine/KlassiRahaHaldamine/Views/Students/CreateUpdateStudent.xaml.cs
@@ -8,8 +8,8 @@ public partial class CreateUpdateStudent : ContentPage
 {
 	public CreateUpdateStudent()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+    }
     private async void OnBackToStudentsClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new StudentsIndex());
@@ -17,6 +17,30 @@ public partial class CreateUpdateStudent : ContentPage
 
     private async void OnSaveStudentClicked(object sender, EventArgs e)
     {
+        if (string.IsNullOrWhiteSpace(FirstNameEntry.Text))
+        {
+            await DisplayAlert("Viga", "Palun sisesta õplase eesnimi.", "OK");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(LastNameEntry.Text))
+        {
+            await DisplayAlert("Viga", "Palun sisesta õpilase perekonna nimi.", "OK");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(ContactNameEntry.Text))
+        {
+            await DisplayAlert("Viga", "Palun sisesta õpilase kontakti nimi.", "OK");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(ContactNumberEntry.Text))
+        {
+            await DisplayAlert("Viga", "Palun sisesta õpilase kontaktnumber.", "OK");
+            return;
+        }
+
         decimal.TryParse(AmountEntry.Text, out decimal amount);
         int.TryParse(ContactNumberEntry.Text, out int contactNumber);
 
