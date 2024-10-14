8de2d16592c3e95883bf61884d1ff5074376a2b2
stefanrammo <86823284+stefanrammo@users.noreply.github.com>
2024-10-14 12:29:55 +0300

    Fixed merge errors

--- MoneyManager/KlassiRahaHaldamine/KlassiRahaHaldamine/Views/Events/EventsIndex.xaml.cs
+++ MoneyManager/KlassiRahaHaldamine/KlassiRahaHaldamine/Views/Events/EventsIndex.xaml.cs
@@ -1,25 +1,54 @@
 using KlassiRahaHaldamine.Data;
 using System.Collections.ObjectModel;
-namespace KlassiRahaHaldamine.Views.Events;
-public partial class EventsIndex : ContentPage
+using System.Linq;
+
+namespace KlassiRahaHaldamine.Views.Events
 {
-    private DatabaseContext _databaseContext;
-    public ObservableCollection<Event> Events { get; set; }
-    public EventsIndex()
+    public partial class EventsIndex : ContentPage
     {
-        InitializeComponent();
-        _databaseContext = new DatabaseContext();
-        Events = new ObservableCollection<Event>();
-        BindingContext = this;
-    }
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        LoadEvents(); // Refresh the list after creating a new event
-    }
-    private async void LoadEvents()
-    {
-        var events = await _databaseContext.GetAllAsync<Event>();
+        private DatabaseContext _databaseContext;
+        public ObservableCollection<Event> Events { get; set; }
+
+        public EventsIndex()
+        {
+            InitializeComponent();
+            _databaseContext = new DatabaseContext();
+            Events = new ObservableCollection<Event>();
+            BindingContext = this;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            LoadEvents(); // Refresh the list every time the page appears
+        }
+
+        private async void LoadEvents()
+        {
+            var events = await _databaseContext.GetAllAsync<Event>();
+
+            // Sort events by event date
+            var sortedEvents = events
+                .OrderBy(e => e.EventDate < DateTime.Now) // Past events move to the end
+                .ThenBy(e => e.EventDate); // Upcoming events will be displayed by date
+
+            Events.Clear();
+            foreach (var eventItem in sortedEvents)
+            {
+                Events.Add(eventItem);
+            }
+        }
+
+        private async void OnBackEventClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void OnCreateEventClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CreateUpdateEvent());
+            LoadEvents(); // Refreshes the event list every time the page appears
+        }
 
         private async void OnDetailsClicked(object sender, EventArgs e)
         {
@@ -27,57 +56,23 @@ public partial class EventsIndex : ContentPage
             var eventItem = (Event)button.CommandParameter; // Get the selected event object
 
             // Navigate to the EventDetailsPage and pass the event object
-            await Navigation.PushAsync(new EventDetailsPage(eventItem));
-        }
-
-
-        private void OnEditClicked(object sender, EventArgs e)
-        // Sort events by event date
-        var sortedEvents = events
-            .OrderBy(e => e.EventDate < DateTime.Now) // Past events move to the end
-            .ThenBy(e => e.EventDate); // Upcoming events will be displayed by date
-
-        Events.Clear();
-        foreach (var eventItem in sortedEvents)
-        {
-            Events.Add(eventItem);
-        }
-    }
-
-    private async void OnBackEventClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new MainPage());
-    }
-
-    private async void OnCreateEventClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new CreateUpdateEvent());
-        LoadEvents(); // Refreshes the event list every time the page appears
-    }
-    private async void OnDetailsClicked(object sender, EventArgs e)
-    {
-        var eventItem = (Event)((Button)sender).CommandParameter;
-        if (eventItem != null)
-        {
-            // Navigate to the EventDetails page and pass the selected event
             await Navigation.PushAsync(new EventDetails(eventItem));
         }
-    }
 
-    private async void OnEditClicked(object sender, EventArgs e)
-    {
-        //var eventItem = (Event)((Button)sender).CommandParameter;
-        // Open edit view
-        //await Navigation.PushAsync(new EventsCreateUpdate());
-        var button = (Button)sender;
-        var eventItem = (Event)button.CommandParameter;
-        await Navigation.PushAsync(new CreateUpdateEvent(eventItem));
-    }
-    
-    private async void OnDeleteClicked(object sender, EventArgs e)
-    {
-        var eventItem = (Event)((Button)sender).CommandParameter;
-        await Navigation.PushAsync(new DeleteEvent(eventItem));
-        LoadEvents(); // Refresh the list after deleting an event
+        private async void OnEditClicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var eventItem = (Event)button.CommandParameter; // Get the selected event object
+
+            // Navigate to the CreateUpdateEvent page and pass the selected event for editing
+            await Navigation.PushAsync(new CreateUpdateEvent(eventItem));
+        }
+
+        private async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var eventItem = (Event)((Button)sender).CommandParameter;
+            await Navigation.PushAsync(new DeleteEvent(eventItem));
+            LoadEvents(); // Refresh the list after deleting an event
+        }
     }
 }
